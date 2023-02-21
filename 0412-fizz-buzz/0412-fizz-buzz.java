abbfb80db3b3aff2b5db21f665c24982f5c1e9d1
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-02-22 00:31:54 +0530

    Time: 1 ms (99.98%), Space: 48.9 MB (6.08%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,7 +3,7 @@ class Solution {
         ArrayList<String> ans = new ArrayList<>();
         int i=1;
         while(i<=n){
-            if((i % 15)==0){
+            if((i % 3)==0 && ((i % 5)==0)){
                 ans.add("FizzBuzz");
             }
             else if((i % 3)==0){
