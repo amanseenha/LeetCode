d6a500f34fff571237a819a19ab301502b49d68d
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-07-07 18:36:19 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -47,23 +47,25 @@ public class Main
 class Solution
 {
     //Function to merge the arrays.
-    public static void merge(long arr1[], long arr2[], int n, int m) {
-    int i = 0, j = 0;
-    int k = n - 1;
-
-    while (i <= k && j < m) {
-        if (arr1[i] <= arr2[j]) {
-            i++;
-        } else {
-            long temp = arr1[k];
-            arr1[k] = arr2[j];
-            arr2[j] = temp;
-            j++;
-            k--;
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+        // code here 
+        int i = 0;
+        int j = 0;
+        int k = n-1;
+        while(i <= k && j < m){
+            if(arr1[i]>=arr2[j]){
+                long temp = arr1[k];
+                arr1[k] = arr2[j];
+                arr2[j] = temp;
+                j++;
+                k--;
+            }
+            else{
+                i++;
+            }
         }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
     }
-
-    Arrays.sort(arr1);
-    Arrays.sort(arr2);
-}
 }
