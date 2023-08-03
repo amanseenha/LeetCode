f64c0b43bb178b581eeada72c2fd3e2882feae26
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-03 14:04:28 +0530

    Time: 5 ms (50.30%), Space: 41.6 MB (13.63%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,32 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0){
-            ArrayList<String> str = new ArrayList<>();
-            return str;
+            ArrayList<String> list = new ArrayList<>();
+            return list;
         }
-        return solve("",digits);
-    }
-    public static List<String> solve(String p, String up) {
+        return helper("", digits);
         
-        if(up.isEmpty()) {
+    }
+    public List<String> helper(String p, String up){
+        if(up.isEmpty()){
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-        
         int digit = up.charAt(0) - '0';
-        int i=(digit-2)*3;
-        if(digit > 7) {
+        int i = (digit-2)*3;
+        if(digit > 7){
             i+=1;
         }
         int range = i+3;
-        if(digit == 7 || digit == 9) {
+        if(digit==7 || digit==9){
             range+=1;
         }
-        
         ArrayList<String> list = new ArrayList<>();
-                
-        for(; i<range; i++) {
+        for(;i<range;i++){
             char ch = (char)('a' + i);
-            list.addAll(solve(p+ch, up.substring(1)));
+            list.addAll(helper(p+ch, up.substring(1)));
         }
-        
         return list;
-        
     }
 }
