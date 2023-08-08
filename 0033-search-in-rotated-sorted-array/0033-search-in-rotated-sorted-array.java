1a9e0316952367bc6826d3441ae41c24194ed7ec
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-08 08:45:20 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (92.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,24 @@
 class Solution {
-
-  int findPivot(int[] nums) {
-    int start = 0, end = nums.length - 1;
-    while (start <= end) {
-      int middle = start + (end - start) / 2;
-      if (middle < end && nums[middle] > nums[middle + 1]) {
-        return middle;
-      } else if (start < middle && nums[middle] < nums[middle - 1]) {
-        return middle - 1;
-      } else if (nums[middle] <= nums[start]) {
-        end = middle - 1;
-      } else {
-        start = middle + 1;
-      }
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end){
+           int mid = (start + end) / 2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1;
+                }
+                else start = mid + 1;
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else end = mid - 1;
+            }
+        }
+        return -1;
     }
-    return -1;
-  }
-
-  int Bsearch(int[] arr, int k, int start, int end) {
-
-    while (start <= end) {
-      int middle = (start + end) / 2;
-      if (arr[middle] > k) {
-        end = middle - 1;
-      } else if (arr[middle] < k) {
-        start = middle + 1;
-      } else if (arr[middle] == k) {
-        return middle;
-      }
-    }
-    return -1;
-  }
-
-  int search(int[] nums, int target) {
-    int pivot = findPivot(nums);
-    if (pivot == -1) {
-      return Bsearch(nums, target, 0, nums.length - 1);
-    } else if (nums[pivot] == target) {
-      return pivot;
-    } else if (target >= nums[0]) {
-      return Bsearch(nums, target, 0, pivot - 1);
-    }
-    return Bsearch(nums, target, pivot + 1, nums.length - 1);
-  }
 }
