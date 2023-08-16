bea8de308951c5e02c26779f02f3124b21748fb3
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-16 06:30:15 +0530

    Time: 0 ms (100.00%), Space: 41 MB (60.57%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessHead = new ListNode(0);
+        ListNode less = lessHead;
+        
+        ListNode greaterHead = new ListNode(0);
+        ListNode greater = greaterHead;
+        
+        ListNode current = head;
+        
+        while (current != null) {
+            if (current.val < x) {
+                less.next = current;
+                less = less.next;
+            } else {
+                greater.next = current;
+                greater = greater.next;
+            }
+            
+            current = current.next;
+        }
+        greater.next = null;
+        less.next = greaterHead.next;
+        
+        return lessHead.next;
+    }
+}
