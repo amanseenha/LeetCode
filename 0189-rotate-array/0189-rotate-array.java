1557e1ff0e7bf42f472519139ab0912ac45054c6
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-20 13:34:41 +0530

    Time: 1 ms (49.47%), Space: 55.6 MB (70.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,14 +3,22 @@ class Solution {
         int n = arr.length;
         d = d % n;
         d = n - d;
-		int[] temp = Arrays.copyOf(arr, d);
-		for (int i = d; i < n; i++) {
-			arr[i-d] = arr[i];
-		}
-		int j = 0 ;
-		for(int i=n-d; i<n; i++){
-			arr[i] = temp[j];
-			j++;
-		}
+        
+        if (d == 0) {
+            return;
+        }
+        
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        
+        for (int i = d; i < n; i++) {
+            arr[i - d] = arr[i];
+        }
+        
+        for (int i = n - d, j = 0; i < n; i++, j++) {
+            arr[i] = temp[j];
+        }
     }
 }
