b4b491da136bd8ef91978ea9278e66e925cc2e58
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-22 11:33:45 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (24.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        if(nums.length == 1){
-            return 1-nums[0];
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        int arrSum = 0;
+        for(int i: nums){
+            arrSum += i;
         }
-        int maxElement = nums.length;
-        int[] hash = new int[maxElement+1];
-        for(int i=0; i<maxElement; i++){
-            for(int j=0; j<=maxElement; j++){
-                if(nums[i]==j){
-                    hash[j]++;
-                }
-            }
-        }
-        for(int i=0; i<hash.length; i++){
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        return sum-arrSum;
     }
 }
