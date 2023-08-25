d5bfd5db601cef0b6b3dcf6ffd9b5e08e6aaab29
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-26 00:13:12 +0530

    Time: 3 ms (67.13%), Space: 40.9 MB (54.97%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        char[] s1Arr = s1.toCharArray();
-        char[] s2Arr = s2.toCharArray();
-        char[] s3Arr = s3.toCharArray();
-        if(s1Arr.length + s2Arr.length != s3Arr.length) return false;
-        boolean[][] dp = new boolean[s1Arr.length + 1][s2Arr.length + 1];
+        if(s1 == null || s2 == null || s3 == null) return false;
+        int len1 = s1.length(), len2 = s2.length(), len3 = s3.length();
+        if(len1 + len2 != len3) return false;
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        char[] s3Char = s3.toCharArray();
         dp[0][0] = true;
-        for(int i = 1; i <= s1Arr.length; i++)
-            dp[i][0] = dp[i - 1][0] && s1Arr[i - 1] == s3Arr[i - 1];
-        for(int i = 1; i <= s2Arr.length; i++)
-            dp[0][i] = dp[0][i - 1] && s2Arr[i - 1] == s3Arr[i - 1];
-        for(int i = 1; i <= s1Arr.length; i++){
-            for(int j = 1; j <= s2Arr.length; j++){
-                if(s1Arr[i - 1] == s3Arr[i + j - 1]){
-                    dp[i][j] = dp[i - 1][j];
-                }
-                if(s2Arr[j - 1] == s3Arr[i + j - 1]){
-                    dp[i][j] |= dp[i][j - 1];
-                }
+        for(int i = 1; i <= len1; i++){
+            dp[i][0] = dp[i - 1][0] && (s3Char[i - 1] == s1.charAt(i - 1));
+        }
+        for(int i = 1; i <= len2; i++){
+            dp[0][i] = dp[0][i - 1] && (s3Char[i - 1] == s2.charAt(i - 1));
+        }
+        for(int i = 1; i <= len1; i++){
+            for(int j = 1; j <= len2; j++){
+                dp[i][j] = (dp[i][j - 1] && (s3Char[i + j - 1] == s2.charAt(j - 1)))
+                    || (dp[i - 1][j] && (s3Char[i + j - 1] == s1.charAt(i - 1)));
             }
         }
-        return dp[s1Arr.length][s2Arr.length];
+        return dp[len1][len2];
     }
 }
