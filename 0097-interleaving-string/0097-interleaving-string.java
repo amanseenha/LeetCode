80ef1fac96988f78df3c5d80332f7821fab7d430
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2023-08-26 00:13:01 +0530

    Time: 4 ms (57.53%), Space: 40.5 MB (92.33%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,29 +1,25 @@
- class Solution {
- 	public boolean isInterleave(String s1, String s2, String s3) {
- 		char[] arr1 = s1.toCharArray();
- 		char[] arr2 = s2.toCharArray();
- 		char[] arr3 = s3.toCharArray();
- 		if(arr1.length + arr2.length != arr3.length) return false;
- 		boolean[][] dp = new boolean[arr1.length + 1][arr2.length + 1];
- 		dp[0][0] = true;
- 		for(int i = 0; i < arr1.length; i++){
- 			if(arr1[i] == arr3[i])
- 				dp[i + 1][0] = dp[i][0];
- 		}
- 		for(int i = 0; i < arr2.length; i++){
- 			if(arr2[i] == arr3[i])
- 				dp[0][i + 1] = dp[0][i];
- 		}
- 		for(int i = 1; i <= arr1.length; i++){
- 			for(int j = 1; j <= arr2.length; j++){
- 				if(arr3[i + j - 1] == arr1[i - 1]){
- 					dp[i][j] = dp[i - 1][j];
- 				}
- 				if(arr3[i + j - 1] == arr2[j - 1]){
- 					dp[i][j] |= dp[i][j - 1];
- 				}
- 			}
- 		}
- 		return dp[arr1.length][arr2.length];
- 	}
- }
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        char[] s1Arr = s1.toCharArray();
+        char[] s2Arr = s2.toCharArray();
+        char[] s3Arr = s3.toCharArray();
+        if(s1Arr.length + s2Arr.length != s3Arr.length) return false;
+        boolean[][] dp = new boolean[s1Arr.length + 1][s2Arr.length + 1];
+        dp[0][0] = true;
+        for(int i = 1; i <= s1Arr.length; i++)
+            dp[i][0] = dp[i - 1][0] && s1Arr[i - 1] == s3Arr[i - 1];
+        for(int i = 1; i <= s2Arr.length; i++)
+            dp[0][i] = dp[0][i - 1] && s2Arr[i - 1] == s3Arr[i - 1];
+        for(int i = 1; i <= s1Arr.length; i++){
+            for(int j = 1; j <= s2Arr.length; j++){
+                if(s1Arr[i - 1] == s3Arr[i + j - 1]){
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if(s2Arr[j - 1] == s3Arr[i + j - 1]){
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+        return dp[s1Arr.length][s2Arr.length];
+    }
+}
