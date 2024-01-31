56b0ea83710945b3385e512568ffe4802365fb78
Aman Sinha <94823199+amanseenha@users.noreply.github.com>
2024-01-31 23:08:43 +0530

    Time: 25 ms (53.52%), Space: 45.5 MB (10.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,27 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = maxElement(piles);
-        while(low <= high){
+        int low = 1, high = maxPile(piles);
+        while(low<=high){
             int mid = (low+high)/2;
-            if(timeToEat(piles, mid) <=h){
-                high = mid-1;
-            }
+            if(totalTimetoEat(piles, mid)<=h) high = mid-1;
             else low = mid+1;
         }
         return low;
     }
-    int maxElement(int[] piles){
-        int max=0;
+    
+    int maxPile(int[] piles){
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<piles.length; i++){
             if(piles[i]>max) max = piles[i];
         }
         return max;
     }
-    int timeToEat(int[] piles, int hourly){
-        int totTime = 0;
+    
+    int totalTimetoEat(int[] piles, int mid){
+        int totalTime = 0;
         for(int i=0; i<piles.length; i++){
-            totTime += Math.ceil((double)piles[i]/(double)hourly);
+            totalTime += Math.ceil((double)piles[i]/(double)mid);
         }
-        return totTime;
+        return totalTime;
     }
 }
